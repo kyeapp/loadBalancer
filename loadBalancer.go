a9ee89f8ffc4d45712f5f0d56f4ca8bd4d3af2ac
Kevin Yeap <paeynivek@gmail.com>
2017-08-13 02:13:37 -0700

    console refresh
    console refresh with mock testing.

--- loadBalancer.go
+++ loadBalancer.go
@@ -1,13 +1,14 @@
 package main
 
-import "fmt"
 import "time"
+import "fmt"
 import "strings"
+import "math/rand"
 
 type serverNode struct {
 	id int
-	serverLoad float32
-	maxRequestTime float32 // max amount of time it takes to complete a request
+	serverLoad int
+	maxRequestTime int // max amount of time it takes to complete a request
 
 }
 
@@ -26,13 +27,52 @@ type loadBalancer struct {
 type users struct {
 
 
-	maxRequestDelay int //delay before next request is sen[M Ct out.
+	maxRequestDelay int //delay before next request is sent out.
 	
 }
 
+func loadBar(s serverNode) string  {
+	maxLen := 20
+	scale := 100/maxLen
+	barLen := s.serverLoad/scale
+	bar := strings.Repeat("=", barLen)
+	space := strings.Repeat(" ", maxLen-barLen)
+	return fmt.Sprintf("server_%d [%s%s] %d      ", s.id, bar, space, s.serverLoad)
+}
+
+func updateStats(serverNodeList []serverNode) {
+	fmt.Printf("\033[0;0H")
+	for _, serverNode := range serverNodeList {
+		fmt.Println(loadBar(serverNode))
+	}
+}
+
+func mockCpu(serverNodeList []serverNode) {
+	for i := 0; i < len(serverNodeList); i++ {
+		serverNodeList[i].serverLoad = rand.Intn(100)
+	}
+}
+
+func clearScreen() {
+	fmt.Printf("\033[0;0H")
+	for i := 0; i < 50; i ++ {
+		fmt.Println(strings.Repeat(" ", 80))
+	}
+}
+
 func main() {
-	for i := 0; i < 101; i++ {
-		fmt.Printf("\r[%s%s] %v", strings.Repeat("=", i/5), strings.Repeat(" ", 20-(i/5)), i)
-		time.Sleep(200 * time.Millisecond)
+	//setup
+	clearScreen()
+	
+	serverNodeList := []serverNode{
+		serverNode{0, 20, 50},
+		serverNode{1, 50, 100},
+		serverNode{2, 100, 150},
+	}
+
+	for i := 0; i < 10; i++ {
+		updateStats(serverNodeList)
+		time.Sleep(500 * time.Millisecond)
+		mockCpu(serverNodeList)
 	}
 }
